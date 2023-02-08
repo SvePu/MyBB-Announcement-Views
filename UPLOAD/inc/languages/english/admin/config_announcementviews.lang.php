1dded71a2aa0a2d5fd2b9493c8a7b74b3237497b
Sven <SvePu@users.noreply.github.com>
2023-02-09 00:15:59 +0100

    plugin update

--- UPLOAD/inc/languages/english/admin/config_announcementviews.lang.php
+++ UPLOAD/inc/languages/english/admin/config_announcementviews.lang.php
@@ -1,16 +1,17 @@
 <?php
 /*
-* Announcement Views - 1.0 Config Language File English
+* Announcement Views - 1.1 Config Language File English
 * Licensed under GNU/GPL v3
 */
 
 $l['announcementviews'] = "Announcement Views";
 $l['announcementviews_desc'] = "Simple MyBB plugin to count and to display number of announcement visits.";
-$l['announcementviews_settings_title'] = "Announcement Views Settings";
-$l['announcementviews_settings_title_desc'] = "Settings of Announcement Views plugin";
-$l['announcementviews_enable_title'] = "Enable Announcement Views Plugin?";
-$l['announcementviews_enable_title_desc'] = "Set YES to enable it!";
+
+$l['setting_group_announcementviews'] = "Announcement Views Settings";
+$l['setting_group_announcementviews_desc'] = "Settings of Announcement Views plugin";
+
+$l['setting_announcementviews_enable'] = "Enable Announcement Views Plugin?";
+$l['setting_announcementviews_enable_desc'] = "Set YES to enable it!";
 
 $l['announcementviews_uninstall'] = "Announcement Views - Uninstallation";
 $l['announcementviews_uninstall_confirm'] = "Do you wish to drop all Announcement Views plugin data entries from the database?";
-?>
--- UPLOAD/inc/plugins/announcementviews.php
+++ UPLOAD/inc/plugins/announcementviews.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Announcement Views Plugin for MyBB 1.8 - v1.0
+Announcement Views Plugin for MyBB 1.8 - v1.1
 Copyright (C) 2017 SvePu
 
 This program is free software: you can redistribute it and/or modify
@@ -17,147 +17,159 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-if(!defined("IN_MYBB"))
+if (!defined("IN_MYBB"))
 {
-	die("Direct initialization of this file is not allowed.");
+    die("Direct initialization of this file is not allowed.");
 }
 
-$plugins->add_hook('forumdisplay_announcement', 'announcementviews_show');
-$plugins->add_hook('announcements_start', 'announcementviews_run');
+if (defined('IN_ADMINCP'))
+{
+    $plugins->add_hook("admin_config_settings_begin", 'announcementviews_load_lang');
+}
+else
+{
+    $plugins->add_hook('forumdisplay_announcement', 'announcementviews_show');
+    $plugins->add_hook('announcements_start', 'announcementviews_run');
+}
 
 function announcementviews_info()
 {
-	global $db, $lang;
-	$lang->load('config_announcementviews');
-	return array(
-		"name"			=> $db->escape_string($lang->announcementviews),
-		"description"	=> $db->escape_string($lang->announcementviews_desc),
-		"website"		=> "https://github.com/SvePu/MyBB-Announcement-Views",
-		"author"		=> "SvePu",
-		"authorsite"	=> "https://community.mybb.com/user-91011.html",
-		"version"		=> "1.0",
-		"guid" 			=> "",
-		"codename"		=> "announcementviews",
-		"compatibility" => "18*"
-	);
+    global $db, $lang;
+    $lang->load('config_announcementviews');
+    return array(
+        "name"          => $db->escape_string($lang->announcementviews),
+        "description"   => $db->escape_string($lang->announcementviews_desc),
+        "website"       => "https://github.com/SvePu/MyBB-Announcement-Views",
+        "author"        => "SvePu",
+        "authorsite"    => "https://community.mybb.com/user-91011.html",
+        "version"       => "1.1",
+        "codename"      => "announcementviews",
+        "compatibility" => "18*"
+    );
 }
 
 function announcementviews_install()
 {
-	global $mybb, $db, $lang;
-	$lang->load('config_announcementviews');
+    global $mybb, $db, $lang;
+    $lang->load('config_announcementviews');
 
-	if(!$db->field_exists("views", "announcements"))
-	{
-		$db->add_column("announcements", "views", "int(10) NOT NULL default '0'");
-	}
-	
-	$query_add = $db->simple_select("settinggroups", "COUNT(*) as rows");
-	$rows = $db->fetch_field($query_add, "rows");
-    $announcementviews_group = array(
-		"name" 			=>	"announcementviews_settings",
-		"title" 		=>	$db->escape_string($lang->announcementviews_settings_title),
-		"description" 	=>	$db->escape_string($lang->announcementviews_settings_title_desc),
-		"disporder"		=> 	$rows+1,
-		"isdefault" 	=>  0
-	);
-    $gid = $db->insert_query("settinggroups", $announcementviews_group);
-	
-	$announcementviews_setting_array = array(
-		'announcementviews_enable' => array(
-			'title'			=> $db->escape_string($lang->announcementviews_enable_title),
-			'description'  	=> $db->escape_string($lang->announcementviews_enable_title_desc),
-			'optionscode'  	=> 'yesno',
-			'value'        	=> 1,
-			'disporder'		=> 1
-		)
-	);
+    if (!$db->field_exists("views", "announcements"))
+    {
+        $db->add_column("announcements", "views", "int(10) NOT NULL default '0'");
+    }
 
-	foreach($announcementviews_setting_array as $name => $setting)
-	{
-		$setting['name'] = $name;
-		$setting['gid'] = $gid;
+    $group = array(
+        'name'        => 'announcementviews',
+        'title'       => $db->escape_string($lang->setting_group_announcementviews),
+        'description' => $db->escape_string($lang->setting_group_announcementviews_desc),
+        'isdefault'   => 0
+    );
 
-		$db->insert_query('settings', $setting);
-	}
-	
-	rebuild_settings();
+    $query = $db->simple_select('settinggroups', 'MAX(disporder) AS disporder');
+    $disporder = (int)$db->fetch_field($query, 'disporder');
+
+    $group['disporder'] = ++$disporder;
+
+    $gid = (int)$db->insert_query('settinggroups', $group);
+
+    $settings = array(
+        'enable' => array(
+            'optionscode' => 'yesno',
+            'value' => 1
+        )
+    );
+
+    $disporder = 0;
+
+    foreach ($settings as $key => $setting)
+    {
+        $key = "announcementviews_{$key}";
+
+        $setting['name'] = $db->escape_string($key);
+
+        $lang_var_title = "setting_{$key}";
+        $lang_var_description = "setting_{$key}_desc";
+
+        $setting['title'] = $db->escape_string($lang->{$lang_var_title});
+        $setting['description'] = $db->escape_string($lang->{$lang_var_description});
+        $setting['disporder'] = $disporder;
+        $setting['gid'] = $gid;
+
+        $db->insert_query('settings', $setting);
+        ++$disporder;
+    }
+
+    rebuild_settings();
 }
 
 function announcementviews_is_installed()
 {
-	global $mybb;
+    global $mybb;
 
-	if(isset($mybb->settings['announcementviews_enable']))
-	{
-		return true;
-	}
-	return false;
+    if (isset($mybb->settings['announcementviews_enable']))
+    {
+        return true;
+    }
+    return false;
 }
 
 function announcementviews_uninstall()
 {
-	global $mybb, $db;
-	
-	if($mybb->request_method != 'post')
-	{
-		global $page, $lang;
-		$lang->load('config_announcementviews');
-		$page->output_confirm_action('index.php?module=config-plugins&action=deactivate&uninstall=1&plugin=announcementviews', $db->escape_string($lang->announcementviews_uninstall_confirm), $db->escape_string($lang->announcementviews_uninstall));
-	}
-	if($db->field_exists("views", "announcements") && !isset($mybb->input['no']))
-	{
-		$db->drop_column("announcements", "views");
-	}
-	
-	$result = $db->simple_select('settinggroups', 'gid', "name = 'announcementviews_settings'", array('limit' => 1));
-	$annoviews_group = $db->fetch_array($result);
-	
-	if(!empty($annoviews_group['gid']))
-	{
-		$db->delete_query('settinggroups', "gid='{$annoviews_group['gid']}'");
-		$db->delete_query('settings', "gid='{$annoviews_group['gid']}'");
-		rebuild_settings();
-	}
+    global $mybb, $db;
+
+    if ($mybb->request_method != 'post')
+    {
+        global $page, $lang;
+        $lang->load('config_announcementviews');
+        $page->output_confirm_action('index.php?module=config-plugins&action=deactivate&uninstall=1&plugin=announcementviews', $db->escape_string($lang->announcementviews_uninstall_confirm), $db->escape_string($lang->announcementviews_uninstall));
+    }
+
+    $db->delete_query("settinggroups", "name='announcementviews'");
+    $db->delete_query("settings", "name LIKE 'announcementviews%'");
+
+    rebuild_settings();
+
+    if (!isset($mybb->input['no']))
+    {
+        if ($db->field_exists("views", "announcements"))
+        {
+            $db->drop_column("announcements", "views");
+        }
+    }
 }
 
 function announcementviews_activate()
 {
-	require_once MYBB_ROOT."/inc/adminfunctions_templates.php";
-	find_replace_templatesets(
-		"forumdisplay_announcements_announcement",
-		"#" . preg_quote("<td align=\"center\" class=\"{\$bgcolor} forumdisplay_announcement\">-</td>\r\n{\$rating}") . "#i",
-		"<td align=\"center\" class=\"{\$bgcolor} forumdisplay_announcement\">{\$annoviews}</td>\r\n{\$rating}"
-	);
-	
-	
+    require_once MYBB_ROOT . "/inc/adminfunctions_templates.php";
+    find_replace_templatesets("forumdisplay_announcements_announcement", "#" . preg_quote("forumdisplay_announcement\">-</td>\n{\$rating}") . "#i", "forumdisplay_announcement\">{\$annoviews}</td>\n{\$rating}");
 }
 
 function announcementviews_deactivate()
 {
-	require_once MYBB_ROOT."/inc/adminfunctions_templates.php";
-	find_replace_templatesets(
-		"forumdisplay_announcements_announcement",
-		"#" . preg_quote('{$annoviews}') . "#i",
-		'-'
-	);
+    require_once MYBB_ROOT . "/inc/adminfunctions_templates.php";
+    find_replace_templatesets("forumdisplay_announcements_announcement", "#" . preg_quote('{$annoviews}') . "#i", '-');
+}
 
+function announcementviews_load_lang()
+{
+    global $lang;
+    $lang->load('config_announcementviews');
 }
 
 function announcementviews_run()
 {
-	global $mybb, $db, $aid;
-	if($mybb->settings['announcementviews_enable'] == 1)
-	{
-		$db->query("UPDATE ".TABLE_PREFIX."announcements SET views=views+1 WHERE aid='$aid'"); 
-	}	
+    global $mybb, $db, $aid;
+    if ($mybb->settings['announcementviews_enable'] == 1)
+    {
+        $db->query("UPDATE " . TABLE_PREFIX . "announcements SET views=views+1 WHERE aid='$aid'");
+    }
 }
 
 function announcementviews_show()
 {
-	global $mybb, $annoviews, $announcement;
-	if($mybb->settings['announcementviews_enable'] == 1)
-	{
-		$annoviews = my_number_format($announcement['views']);
-	}
+    global $mybb, $annoviews, $announcement;
+    if ($mybb->settings['announcementviews_enable'] == 1)
+    {
+        $annoviews = my_number_format($announcement['views']);
+    }
 }
